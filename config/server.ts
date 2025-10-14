57a61940b546ed2f52087fc68a7b1dad0eb6f49c
Daniel1234Daniel <dtekleyohannes@gmail.com>
2025-10-14 21:30:38 +0300

    server.ts update again for some change

--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,21 @@
 "use strict";
 
-    module.exports = ({ env }) => ({
-      host: env('HOST', '0.0.0.0'),
-      port: env.int('PORT', 1337),
-     
-      proxy: true, 
-      app: {
-        keys: env.array('APP_KEYS'),
-      },
-    });
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+
+  url: env('PUBLIC_URL', 'https://season-master-strapi.onrender.com'),
+
+  proxy: true, 
+
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+    url: '/admin',
+  },
+});
