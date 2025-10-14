f9e9bd9a0bdfb2c2c75c33a1665cd501319300a2
Daniel1234Daniel <dtekleyohannes@gmail.com>
2025-10-14 23:04:15 +0300

    simple change

--- src/middlewares/log-request.ts
+++ src/middlewares/log-request.ts
@@ -1,6 +1,6 @@
-module.exports = (ctx, next) => {
-  console.log('Request protocol:', ctx.request.protocol);
-  console.log('X-Forwarded-Proto:', ctx.request.header['x-forwarded-proto']);
-  console.log('COOKIE_SECURE:', process.env.COOKIE_SECURE);
-  return next();
+module.exports = async (ctx, next) => {
+  if (ctx.request && ctx.request.protocol) {
+    console.log(ctx.request.protocol);
+  }
+  await next();
 };
