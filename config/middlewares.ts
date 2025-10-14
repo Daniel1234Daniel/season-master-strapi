6f91e5d060e8931663e8b070eb3ad07d1c1df3ef
Daniel1234Daniel <dtekleyohannes@gmail.com>
2025-10-14 22:45:53 +0300

    logs

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,4 +1,10 @@
 export default [
+    async (ctx, next) => {
+    console.log('Request protocol:', ctx.request.protocol);
+    console.log('X-Forwarded-Proto:', ctx.request.header['x-forwarded-proto']);
+    console.log('COOKIE_SECURE:', process.env.COOKIE_SECURE);
+    await next();
+  },
   'strapi::logger',
   'strapi::errors',
  {
