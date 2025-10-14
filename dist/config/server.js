bf6a0641734bfff84c808e144e47d02d64437ae4
Daniel1234Daniel <dtekleyohannes@gmail.com>
2025-10-14 19:14:18 +0300

    server changed in order to fix the render issue

--- dist/config/server.js
+++ dist/config/server.js
@@ -1,22 +1,22 @@
 "use strict";
-
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-
-  url: env('PUBLIC_URL', 'https://season-master-strapi.onrender.com'), 
-
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-
-  proxy: true, 
-
-  admin: {
-    auth: {
-      options: {
-        secure: false, 
-      },
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    url: env('PUBLIC_URL', 'https://season-master-strapi.onrender.com'),
+    app: {
+        keys: env.array('APP_KEYS'),
     },
-  },
+    proxy: true,
+    admin: {
+        auth: {
+            secret: env('ADMIN_JWT_SECRET'),
+        },
+        url: '/admin',
+    },
+    settings: {
+        cors: {
+            enabled: true,
+            origin: ['https://season-master-strapi.onrender.com'],
+        },
+    },
 });
